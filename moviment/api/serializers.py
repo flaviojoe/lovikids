6c8feb0dfd1003dfbffa61e0f6f967cc5b9350b7
Flavio Moraes <flaviojoe@gmail.com>
2022-09-28 11:24:31 -0300

    estou na parte da api, ainda...

--- moviment/api/serializers.py
+++ moviment/api/serializers.py
@@ -1,15 +1,11 @@
-from dataclasses import fields
-import imp
-from pyclbr import Class
-from pyexpat import model
-from urllib.request import CacheFTPHandler
-from rest_framework.serializers import ModelSerializer, StringRelatedField
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
 from moviment.models import JobExec
+from core.models import Job
 
 
 class JobExecSerializer(ModelSerializer):
-    name = StringRelatedField()
+    # job = PrimaryKeyRelatedField(queryset=Job.objects.select_related('name').all())
 
     class Meta:
         model = JobExec
-        fields = ['id', 'name']
+        fields = ['id', 'description']
